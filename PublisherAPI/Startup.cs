c13674ed5cb2701b8a320ba80b0552403e5e4e5c
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-31 18:45:28 -0400

    Initial commit through Manage User Secrets.

--- PublisherAPI/Startup.cs
+++ PublisherAPI/Startup.cs
@@ -7,11 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace PublisherAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,9 +40,11 @@ namespace PublisherAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            var message = Configuration["Message"];
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(message);
             });
         }
     }
